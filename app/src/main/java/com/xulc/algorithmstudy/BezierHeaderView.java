b37d72e42973f4d491c4a8f0f8d8fa2f3073945b
xuliangchun <xulc_sky@yeah.net>
2017-12-21 11:39:55 +0800

    贝塞尔动画header

--- app/src/main/java/com/xulc/algorithmstudy/BezierHeaderView.java
+++ app/src/main/java/com/xulc/algorithmstudy/BezierHeaderView.java
@@ -1,31 +1,46 @@
 package com.xulc.algorithmstudy;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+
 
 /**
  * Date：2017/12/20
- * Desc：
+ * Desc：贝塞尔自定义header
  * Created by xuliangchun.
  */
 
 public class BezierHeaderView extends LinearLayout implements StudyRefreshView.IHeaderCallBack{
-    private TextView tvLoadTip;
+    private ImageView ivInlife;
+    private int startRotateDistance = 160;//inlife开始作旋转操作的临界位置点
     private BezierView bezierView;
     private boolean isRefreshFinishMove;
+    private Animation scaleAnimation;
     public BezierHeaderView(Context context) {
         this(context,null);
     }
 
     public BezierHeaderView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
         LayoutInflater.from(context).inflate(R.layout.layout_bezier_header,this,true);
         bezierView = findViewById(R.id.bezierView);
-        tvLoadTip = findViewById(R.id.tvLoadTip);
+        ivInlife = findViewById(R.id.ivInlife);
+        //定义缩放动画
+        scaleAnimation = new ScaleAnimation(1.0f,1.4f,1.0f,1.4f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        scaleAnimation.setDuration(200);
+        scaleAnimation.setRepeatCount(-1);
+        scaleAnimation.setRepeatMode(Animation.REVERSE);
+        //初始状态inlife
+        ivInlife.setScaleX(0f);
+        ivInlife.setScaleY(0f);
     }
 
 
@@ -33,6 +48,7 @@ public class BezierHeaderView extends LinearLayout implements StudyRefreshView.I
     @Override
     public void onStateRefresh(int headerHeight) {
         isRefreshFinishMove = true;
+        ivInlife.startAnimation(scaleAnimation);
     }
 
     @Override
@@ -48,19 +64,56 @@ public class BezierHeaderView extends LinearLayout implements StudyRefreshView.I
 
     @Override
     public void onHeaderMove(int headerHeight, int offsetY) {
-        //刷新完成后的头部位移不需要通知贝塞尔
-        if (!isRefreshFinishMove&&offsetY<=headerHeight){
-            bezierView.setOffsetY(offsetY);
+        //刷新完成后的头部位移不需要通知贝塞尔，贝塞尔的绘制注意偏移量要控制在headerHeight以内
+        if (!isRefreshFinishMove){
+            bezierView.setOffsetY(Math.min(headerHeight,offsetY));
+            //下拉到某个临界位置时，开始对inlife进行旋转
+            if (offsetY>=startRotateDistance){
+                //接下来到真正能够刷新的距离做180等分
+                //显示图标
+                ivInlife.setScaleX(1.0f);
+                ivInlife.setScaleY(1.0f);
 
+                //加一个容错
+                if (offsetY>=headerHeight){
+                    ivInlife.setRotation(180);
+                }else {
+                    int angle = 180*(offsetY-startRotateDistance)/(headerHeight-startRotateDistance);
+                    ivInlife.setRotation(angle);
+                }
+            }
         }
+
+        //重置一些条件
         if (offsetY==0){
             isRefreshFinishMove = false;
+            //图标要回到初始位置 初始状态
+            ObjectAnimator anim = ObjectAnimator.ofFloat(ivInlife, "translationY", 0, 0);
+            anim.start();
+            ivInlife.setRotation(0);
+        }
+
+
+        //在达到inlife旋转的临界值前，进行一次0-1的展现
+        if (offsetY<startRotateDistance){
+            float alpha = (float) offsetY/(float) startRotateDistance;
+            ivInlife.setScaleX(alpha);
+            ivInlife.setScaleY(alpha);
         }
 
     }
 
+    /**
+     * 刷新完成
+     * @param studyRefreshView
+     */
     public void stopRefresh(StudyRefreshView studyRefreshView){
+        ivInlife.clearAnimation();
         bezierView.stopRefresh(studyRefreshView);
+        //刷新完成后 图标也要往上位移
+        ObjectAnimator anim = ObjectAnimator.ofFloat(ivInlife, "translationY", 0, -getHeight());
+        anim.setDuration(250);
+        anim.start();
     }
 
 }
