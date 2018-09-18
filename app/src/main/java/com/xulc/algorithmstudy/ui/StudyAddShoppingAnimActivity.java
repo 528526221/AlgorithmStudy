0f6d801bc14fee6c4994a0bae48d4899d3dc945b
xuliangchun <xulc_sky@yeah.net>
2018-09-18 12:01:09 +0800

    利用Xfermode混合模式实现圆形图片和圆角图片

--- app/src/main/java/com/xulc/algorithmstudy/ui/StudyAddShoppingAnimActivity.java
+++ app/src/main/java/com/xulc/algorithmstudy/ui/StudyAddShoppingAnimActivity.java
@@ -6,8 +6,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.bumptech.glide.Glide;
 import com.xulc.algorithmstudy.R;
 import com.xulc.algorithmstudy.util.AddShoppingAnimUtil;
+import com.xulc.algorithmstudy.widget.RoundImageView;
+
 
 /**
  * Date：2018/5/22
@@ -17,6 +20,7 @@ import com.xulc.algorithmstudy.util.AddShoppingAnimUtil;
 
 public class StudyAddShoppingAnimActivity extends BaseActivity implements View.OnClickListener, AddShoppingAnimUtil.AddShoppingAnimListener {
     private Button btnStart1,btnStart2,btnStart3,btnEnd;
+    private RoundImageView roundIv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +28,7 @@ public class StudyAddShoppingAnimActivity extends BaseActivity implements View.O
         btnStart1 = (Button) findViewById(R.id.btnStart1);
         btnStart2 = (Button) findViewById(R.id.btnStart2);
         btnStart3 = (Button) findViewById(R.id.btnStart3);
-
+        roundIv = (RoundImageView) findViewById(R.id.roundIv);
         btnEnd = (Button) findViewById(R.id.btnEnd);
         btnStart1.setOnClickListener(this);
         btnStart2.setOnClickListener(this);
@@ -60,5 +64,15 @@ public class StudyAddShoppingAnimActivity extends BaseActivity implements View.O
     protected void onDestroy() {
         AddShoppingAnimUtil.stopAnim();
         super.onDestroy();
+    }
+
+    public void changePicture(View view) {
+        if (view.getTag() == null){
+            view.setTag(1);
+            Glide.with(this).load("https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2049667773,1018633067&fm=26&gp=0.jpg").into(roundIv);
+        }else {
+            view.setTag(null);
+            Glide.with(this).load("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2804169900,3992834783&fm=26&gp=0.jpg").into(roundIv);
+        }
     }
 }
--- app/src/main/java/com/xulc/algorithmstudy/widget/RoundImageView.java
+++ app/src/main/java/com/xulc/algorithmstudy/widget/RoundImageView.java
@@ -5,15 +5,19 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
 import android.graphics.Xfermode;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 
 import com.xulc.algorithmstudy.R;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Date：2018/9/17
@@ -21,96 +25,159 @@ import com.xulc.algorithmstudy.R;
  * Created by xulc.
  */
 
-public class RoundImageView extends android.support.v7.widget.AppCompatImageView{
+public class RoundImageView extends android.support.v7.widget.AppCompatImageView {
     private Paint mPaint;
     private int roundType;//类型 圆或者圆角
     private float rx;//圆角x-radius
     private float ry;//圆角y-radius
-    private Xfermode xfermode;
+    private Xfermode xfermode;//过渡模式
+    private WeakReference<Bitmap> bitmapClip;//裁剪后的bitmap
+    private WeakReference<Bitmap> bitmap;//drawable转化成bitmap
+    private RectF rectF;
+
     public RoundImageView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public RoundImageView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.RoundImageView);
-        roundType = array.getInt(R.styleable.RoundImageView_RoundType,1);
-        rx = array.getDimension(R.styleable.RoundImageView_CornerX,0);
-        ry = array.getDimension(R.styleable.RoundImageView_CornerY,0);
+        roundType = array.getInt(R.styleable.RoundImageView_RoundType, 1);
+        rx = array.getDimension(R.styleable.RoundImageView_CornerX, 0);
+        ry = array.getDimension(R.styleable.RoundImageView_CornerY, 0);
         array.recycle();
-
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
         xfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_IN);
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        clearBitmap();
+    }
+
+
+    @Override
     protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
-        BitmapDrawable drawable = (BitmapDrawable) getDrawable();
-
-        Bitmap bitmap = drawable.getBitmap();
-
+        if (getDrawable() == null)
+            return;
+        if (bitmap == null || bitmap.get() == null) {
+            bitmap = new WeakReference<Bitmap>(drawableToBitmap(getDrawable()));
+        }
         int width = getWidth();
         int height = getHeight();
-        int bWidth = bitmap.getWidth();
-        int bHeight = bitmap.getHeight();
+        int bWidth = bitmap.get().getWidth();
+        int bHeight = bitmap.get().getHeight();
 
-        if (roundType == RoundType.CIRCLE.value){
+        if (roundType == 1) {
             //1.处理圆形
 
             //裁剪圆形图片
-            Bitmap bitmapClip = null;
-            if (bHeight > bWidth){
-                bitmapClip = Bitmap.createBitmap(bitmap,0,(bHeight-bWidth)/2,bWidth,bWidth);
-            }else {
-                bitmapClip = Bitmap.createBitmap(bitmap,(bWidth-bHeight)/2,0,bHeight,bHeight);
+            if (bitmapClip == null || bitmapClip.get() == null) {
+                if (bHeight > bWidth) {
+                    bitmapClip = new WeakReference<Bitmap>(Bitmap.createBitmap(bitmap.get(), 0, (bHeight - bWidth) / 2, bWidth, bWidth));
+                } else {
+                    bitmapClip = new WeakReference<Bitmap>(Bitmap.createBitmap(bitmap.get(), (bWidth - bHeight) / 2, 0, bHeight, bHeight));
+                }
             }
-            int min = Math.min(width,height);
-            //缩放图片尺寸
-            Bitmap bitmapScale = Bitmap.createScaledBitmap(bitmapClip,min,min,true);
-            int saveCount = canvas.saveLayer(0,0,width,height,mPaint,Canvas.ALL_SAVE_FLAG);
-            canvas.drawCircle(width/2f,height/2f,min/2f,mPaint);
+            int min = Math.min(width, height);
+            //混合模式绘制
+            int saveCount = canvas.saveLayer(0, 0, width, height, mPaint, Canvas.ALL_SAVE_FLAG);
+            canvas.drawCircle(width / 2f, height / 2f, min / 2f, mPaint);
             mPaint.setXfermode(xfermode);
-            canvas.drawBitmap(bitmapScale,(width-bitmapScale.getWidth())/2f,(height-bitmapScale.getHeight())/2f,mPaint);
+            canvas.drawBitmap(bitmapClip.get(), (width - bitmapClip.get().getWidth()) / 2f, (height - bitmapClip.get().getHeight()) / 2f, mPaint);
             mPaint.setXfermode(null);
             canvas.restoreToCount(saveCount);
-
-        }else {
+        } else {
             //2.处理圆角
 
             //裁剪合适尺寸
-            Bitmap bitmapClip = null;
-            if (bWidth*1.0f/bHeight > width*1.0f/height){
-                //图片比控件的宽高比要大，那么把图片的横向裁剪一部分
-                int dstBWidth = bHeight*width/height;
-                bitmapClip = Bitmap.createBitmap(bitmap,(bWidth-dstBWidth)/2,0,dstBWidth,bHeight);
-            }else {
-                //裁剪图片纵向的一部分
-                int dstBHeight = bWidth*height/width;
-                bitmapClip = Bitmap.createBitmap(bitmap,0,(bHeight-dstBHeight)/2,bWidth,dstBHeight);
+            if (bitmapClip == null || bitmapClip.get() == null) {
+                if (bWidth > width) {
+                    //裁剪图片横向的一部分
+                    int dstBWidth = bHeight * width / height;
+                    bitmapClip = new WeakReference<Bitmap>(Bitmap.createBitmap(bitmap.get(), (bWidth - dstBWidth) / 2, 0, dstBWidth, bHeight));
+                } else {
+                    //裁剪图片纵向的一部分
+                    int dstBHeight = bWidth * height / width;
+                    bitmapClip = new WeakReference<Bitmap>(Bitmap.createBitmap(bitmap.get(), 0, (bHeight - dstBHeight) / 2, bWidth, dstBHeight));
+                }
             }
-            Bitmap bitmapScale = Bitmap.createScaledBitmap(bitmapClip,width,height,true);
-            int saveCount = canvas.saveLayer(0,0,width,height,mPaint,Canvas.ALL_SAVE_FLAG);
-            canvas.drawRoundRect(new RectF(0,0,width,height),rx,ry,mPaint);
+            //混合模式绘制
+            int saveCount = canvas.saveLayer(0, 0, width, height, mPaint, Canvas.ALL_SAVE_FLAG);
+            if (rectF == null){
+                rectF = new RectF(0,0,width,height);
+            }
+            canvas.drawRoundRect(rectF, rx, ry, mPaint);
             mPaint.setXfermode(xfermode);
-            canvas.drawBitmap(bitmapScale,0,0,mPaint);
+            canvas.drawBitmap(bitmapClip.get(), 0, 0, mPaint);
             mPaint.setXfermode(null);
             canvas.restoreToCount(saveCount);
         }
 
     }
 
-    enum RoundType{
-        CIRCLE(1),
-        CORNER(2);
-        private int value;
+    /**
+     * 根据drawable获取一个缩放后宽高大于控件的bitmap
+     *
+     * @param drawable
+     * @return
+     */
+    public Bitmap drawableToBitmap(Drawable drawable) {
+        // 取 drawable 的长宽
+        int dWidth = drawable.getIntrinsicWidth();
+        int dHeight = drawable.getIntrinsicHeight();
+        //控件的宽高
+        int width = getWidth();
+        int height = getHeight();
+        //先把图片的宽高缩放到控件适配，缩放后的图片宽高，一定要大于等于控件的宽高
+        float scale = Math.max(width * 1.0f / dWidth, height * 1.0f / dHeight);
 
-        RoundType(int value) {
-            this.value = value;
+        // 取 drawable 的颜色格式
+        Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+                : Bitmap.Config.RGB_565;
+        // 建立对应 bitmap
+        Bitmap bitmap = Bitmap.createBitmap((int) (dWidth * scale), (int) (dHeight * scale), config);
+        // 建立对应 bitmap 的画布
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, (int) (dWidth * scale), (int) (dHeight * scale));
+        // 把 drawable 内容画到画布中
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    @Override
+    public void setImageBitmap(Bitmap bm) {
+        super.setImageBitmap(bm);
+        clearBitmap();
+    }
+
+    private void clearBitmap() {
+        if (bitmap != null) {
+            bitmap.clear();
         }
+        if (bitmapClip != null) {
+            bitmapClip.clear();
+        }
+        if (rectF != null){
+            rectF = null;
+        }
+    }
 
+    @Override
+    public void setImageResource(@DrawableRes int resId) {
+        super.setImageResource(resId);
+        clearBitmap();
 
     }
+
+    @Override
+    public void setImageDrawable(@Nullable Drawable drawable) {
+        super.setImageDrawable(drawable);
+        clearBitmap();
+    }
+
+
 }
